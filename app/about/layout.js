14afe958573595f1236922d65e0bd80e766c886e
Jisun696 <istiakkashem35@gmail.com>
2024-08-26 02:24:58 +0600

    404 not found page

--- app/about/layout.js
+++ app/about/layout.js
@@ -7,7 +7,7 @@ export default function AboutLayout({ children }) {
       <div className="flex justify-between">
         <h1 className="text-center text-indigo-500">About Page Layout</h1>
         <nav>
-          <Link href={'/about/mission'}>Mission</Link> | <Link href={'/about/vision'}>Vision</Link>
+          <Link href={'/about/mission'}>Mission</Link> | <Link href={'/about/vision'}>Vision</Link> | <Link href={'/about/goals'}>Goals</Link>
         </nav>
       </div>
       <hr className="my-2" />
---
+++ app/not-found.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotFound() {
+  return <div className="text-center text-red-200 text-lg">404! Sorry! The Requested Resource Was Not Found</div>;
+}
