52f4027e1913cfbf66cc1bb556705acd4fac916c
Jisun696 <istiakkashem35@gmail.com>
2024-08-27 22:53:48 +0600

    image optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,10 +1,13 @@
 import Button from '@/app/components/Button';
 import React from 'react';
+import thumb from '@/public/images/thumb.webp';
+import Image from 'next/image';
 
 export default function Mission() {
   return (
     <div>
       <h1 className="font-semibold text-center mb-3">Our Mission</h1>
+      <Image src={thumb} alt="thumb" placeholder="blur" className="mx-auto" />
       <p className="text-sm text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta iusto, possimus necessitatibus ut cumque. Ex architecto facere sequi, quod vero dolore saepe illum
         incidunt, dolor non quis ipsam blanditiis. Deleniti quas temporibus dolore odit, rerum quis perferendis tempora aperiam, tempore labore dolores fugiat natus iusto quo
