92ada6fd893975b69009340dae0c9e18c0ec1627
Jisun696 <istiakkashem35@gmail.com>
2024-09-01 22:29:06 +0600

    redirects

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
+// import { redirect } from 'next/navigation';
+
+// const user = true;
+
 export default async function Home() {
   const res = await fetch('http://localhost:3000/time', { cache: 'no-store' });
   const data = await res.json();
+
+  // if (user) {
+  //   redirect('/posts');
+  // }
 
   return (
     <main>
