4d4179f3f66795ae961e2e6b20d29c7f76000944
Jisun696 <istiakkashem35@gmail.com>
2024-09-02 22:28:32 +0600

    nextAuth login | session-token

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,35 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 const handler = NextAuth({
   session: {
     strategy: 'jwt', //default
   },
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: 'Email', type: 'text', required: true, placeholder: 'Your Email' },
+        password: { label: 'Password', type: 'password', required: true, placeholder: 'Enter Password' },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        if (!credentials) return null;
+
+        if (email) {
+          const currentUser = users.find((user) => user.email === email);
+          // console.log('currentUser=> ', currentUser);
+          if (currentUser && currentUser.password === password) return currentUser;
+        }
+        return null;
+      },
+    }),
+  ],
 });
+
+const users = [
+  { id: 1, name: 'John Doe', email: 'john.doe@example.com', password: 'password123' },
+  { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', password: 'secret123' },
+  { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com', password: 'qwerty123' },
+];
 
 export { handler as GET, handler as POST };
