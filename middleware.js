d3394dc25784cb7f20a8ca5b6668d0eb76142404
Jisun696 <istiakkashem35@gmail.com>
2024-09-03 23:08:48 +0600

    protect route

--- middleware.js
+++ middleware.js
@@ -1,25 +1,30 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-const user = true;
-const coo = 'next-superhero123';
+// const user = true;
+// const coo = 'next-superhero123';
 
-export const middleware = (request) => {
+export const middleware = async (request) => {
+  console.log('request from middleware => ', request.url);
   // if (request.nextUrl.pathname.startsWith('/about')) {
   //   return NextResponse.rewrite(new URL('/categories', request.url));
   // }
 
   // const cookies = request.cookies.get('token');
   // if (!cookies || cookies.value !== coo) return NextResponse.redirect(new URL('/login', request.url));
+  const token = cookies(request).get('next-auth.session-token');
+  console.log('token from middleware => ', token);
+  if (!token) {
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url));
+  }
+  return NextResponse.next(); // if user available then go to desired private route
 
-  if (!user) return NextResponse.redirect(new URL('/login', request.url));
-  // return NextResponse.next() // if user available then go to desired private route
-
-  return NextResponse.redirect(new URL('/posts', request.url));
+  // return NextResponse.redirect(new URL('/posts', request.url));
   // return NextResponse.rewrite(new URL('/posts', request.url));
 };
 
 export const config = {
-  // matcher: ['/dashboard/:path*'],  all related to dashboard
+  matcher: ['/dashboard/:path*'], // all related to dashboard
   // matcher: ['/user', '/about'],
-  matcher: '/user',
+  // matcher: '/user',
 };
